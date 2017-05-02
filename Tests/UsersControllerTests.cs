6c6a4d54613ce49497ae6e033a1567ed5a875a9d
Oleksandr Dudik <oleksandr.dudik@sigma.software.com>
2017-05-02 20:02:05 +0300

    Add first unit test

---
+++ Tests/UsersControllerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests
+{
+    using System.Web.Http.Results;
+    using FluentAssertions;
+    using NSubstitute;
+    using WebApi;
+    using WebApi.Controllers;
+    using Xunit;
+
+    public class UsersControllerTests
+    {
+        [Fact]
+        public void should_say_hello_from_server()
+        {
+            //Arrange
+            var hello = Substitute.For<IHello>();
+            hello.SayHello("Sasha").Returns("Hello Sasha");
+            var controller = new UsersController(hello);
+
+            //Act
+            var result = controller.Get() as OkNegotiatedContentResult<string>; 
+
+            //Assert     
+            result.Should().NotBeNull();
+            result.Content.Should().Be("Hello Sasha");
+        }
+    }
+}

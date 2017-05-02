eeb19b20feaefe2ae725bc2c9ab808bf4a7d2c7d
Oleksandr Dudik <oleksandr.dudik@sigma.software.com>
2017-05-02 19:44:35 +0300

    Add tests infrastructure

---
+++ Tests/NSubAutoDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests
+{
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoNSubstitute;
+    using Ploeh.AutoFixture.Xunit2;
+
+    public class NSubAutoDataAttribute : AutoDataAttribute
+    {
+        public NSubAutoDataAttribute()
+            : base(GetFixture())
+        {
+        }
+
+        private static IFixture GetFixture()
+        {
+            var fixture = new Fixture();
+
+            //These 2 lines of code for proper Autofixture generation of parent-child graphs 
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            fixture.Customize(new AutoNSubstituteCustomization());
+
+            return fixture;
+        }
+    }
+}

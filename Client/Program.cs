be89615b982b76f5bb234d3f1948d940a70a5361
Oleksandr Dudik <oleksandr.dudik@sigma.software.com>
2017-05-07 14:09:34 +0300

    Call WCF service from client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace Client
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var service = new WcfService.UserServiceClient();
+
+            Console.WriteLine("Input user name:");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Input company name:");
+            var companyName = Console.ReadLine();
+
+            service.AddUser(userName, companyName);
+
+            Console.ReadKey();
         }
     }
 }
